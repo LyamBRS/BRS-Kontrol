a00f7c713010eaaf7d2afb150a58c6085fb48488
LyamBRS <=>
2023-04-18 15:22:02 -0400

    RPI resolution test 2

--- Application.py
+++ Application.py
@@ -9,17 +9,6 @@ LoadingLog.Start("Application.py")
 # Imports
 #===================================================================#
 LoadingLog.Import("Kivy")
-# To change the kivy default settings
-# we use this module config
-from kivy.config import Config
-
-# 0 being off 1 being on as in true / false
-# you can use 0 or 1 && True or False
-Config.set('graphics', 'resizable', '0')
-
-# fix the width of the window
-Config.set('graphics', 'width', '1024')
-Config.set('graphics', 'height', '600')
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition, CardTransition
 # -------------------------------------------------------------------
@@ -104,7 +93,7 @@ class Application(MDApp):
         #Window.resizable = True
         #Window.left = -1024
         #Window.top = 600
-        #Window.fullscreen = "auto"
+        Window.fullscreen = "auto"
 
         # Create screen manager
         Debug.Log("Creating ScreenManager()")

165206161088926f51e9cf06e8362b1a41b078ce
LyamBRS <=>
2023-04-18 18:11:02 -0400

    Forgot something

--- Application.py
+++ Application.py
@@ -42,7 +42,17 @@ ButtonFont.size = "32sp"
 #====================================================================#
 # Functions
 #====================================================================#
+# To change the kivy default settings
+# we use this module config
+from kivy.config import Config
 
+# 0 being off 1 being on as in true / false
+# you can use 0 or 1 && True or False
+Config.set('graphics', 'resizable', '0')
+
+# fix the width of the window
+Config.set('graphics', 'width', '1024')
+Config.set('graphics', 'height', '600')
 #====================================================================#
 # Classes
 #====================================================================#

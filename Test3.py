ab3683e3ffecb222c82dd2c9a2107f3668eed395
LyamBRS <=>
2023-05-15 11:29:11 -0400

    Tests for WiFi detection on Linux devices

--- Test3.py
+++ Test3.py
@@ -1,79 +1,88 @@
+from Libraries.BRS_Python_Libraries.BRS.Debug.consoleLog import Debug
+from Libraries.BRS_Python_Libraries.BRS.Utilities.Information import Information
+from Libraries.BRS_Python_Libraries.BRS.Utilities.Enums import Execution
+
+
+Debug.enableConsole = True
+result = Lniux_GetWiFiNetworks()
+Debug.Log(str(result))
+
 # import module
-import os
-
-name = ""
-
+# import os
+# 
+# name = ""
+# 
 # function to establish a new connection
-def createNewConnection(name, SSID, password):
-    config = f"""<?xml version=\"1.0\"?>
-<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
-    <name>{name}</name>
-    <SSIDConfig>
-        <SSID>
-            <name>{SSID}</name>
-        </SSID>
-    </SSIDConfig>
-    <connectionType>ESS</connectionType>
-    <connectionMode>auto</connectionMode>
-    <MSM>
-        <security>
-            <authEncryption>
-                <authentication>WPA2PSK</authentication>
-                <encryption>AES</encryption>
-                <useOneX>false</useOneX>
-            </authEncryption>
-            <sharedKey>
-                <keyType>passPhrase</keyType>
-                <protected>false</protected>
-                <keyMaterial>{password}</keyMaterial>
-            </sharedKey>
-        </security>
-    </MSM>
-</WLANProfile>"""
-
-    print(">>> >>> Config: ")
-    print(f">>> >>> {config}")
-    print(">>> >>>  Command: ")
-    command = f"netsh wlan add profile filename=\""+name+".xml\""+" interface=\"Wi-Fi\""
-    print(command)
-    with open(name+".xml", 'w') as file:
-        print(">>> >>> WRITING CONFIG IN XML")
-        file.write(config)
-    os.system(command)
-
+# def createNewConnection(name, SSID, password):
+    # config = f"""<?xml version=\"1.0\"?>
+# <WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+    # <name>{name}</name>
+    # <SSIDConfig>
+        # <SSID>
+            # <name>{SSID}</name>
+        # </SSID>
+    # </SSIDConfig>
+    # <connectionType>ESS</connectionType>
+    # <connectionMode>auto</connectionMode>
+    # <MSM>
+        # <security>
+            # <authEncryption>
+                # <authentication>WPA2PSK</authentication>
+                # <encryption>AES</encryption>
+                # <useOneX>false</useOneX>
+            # </authEncryption>
+            # <sharedKey>
+                # <keyType>passPhrase</keyType>
+                # <protected>false</protected>
+                # <keyMaterial>{password}</keyMaterial>
+            # </sharedKey>
+        # </security>
+    # </MSM>
+# </WLANProfile>"""
+# 
+    # print(">>> >>> Config: ")
+    # print(f">>> >>> {config}")
+    # print(">>> >>>  Command: ")
+    # command = f"netsh wlan add profile filename=\""+name+".xml\""+" interface=\"Wi-Fi\""
+    # print(command)
+    # with open(name+".xml", 'w') as file:
+        # print(">>> >>> WRITING CONFIG IN XML")
+        # file.write(config)
+    # os.system(command)
+# 
 # function to connect to a network
-def connect(name, SSID):
-    print(">>> >>> Command:")
-    command = "netsh wlan connect name=\""+name+"\" ssid=\""+SSID+"\""
-    print(f">>> >>> {command}")
-    os.system(command)
-
+# def connect(name, SSID):
+    # print(">>> >>> Command:")
+    # command = "netsh wlan connect name=\""+name+"\" ssid=\""+SSID+"\""
+    # print(f">>> >>> {command}")
+    # os.system(command)
+# 
 # function to display avavilabe Wifi networks
-def displayAvailableNetworks():
-    print(">>> >>> displayAvailableNetworks: Command: ")
-    command = "netsh wlan show networks interface=\"Wi-Fi\""
-    print(f">>> >>> {command}")
-    os.system(command)
-
-
+# def displayAvailableNetworks():
+    # print(">>> >>> displayAvailableNetworks: Command: ")
+    # command = "netsh wlan show networks interface=\"Wi-Fi\""
+    # print(f">>> >>> {command}")
+    # os.system(command)
+# 
+# 
 # display available netwroks
-print(">>> DISPLAYING AVAILABLE NETWORKS")
-displayAvailableNetworks()
-print(">>> DISPLAY FINISHED.")
-
+# print(">>> DISPLAYING AVAILABLE NETWORKS")
+# displayAvailableNetworks()
+# print(">>> DISPLAY FINISHED.")
+# 
 # input wifi name and password
-name = "Batiscan"
-password = "BATISCAN"
-
+# name = "Batiscan"
+# password = "BATISCAN"
+# 
 # establish new connection
-print(">>> CREATING NEW CONNECTION...")
-createNewConnection(name, name, password)
-print(">>> CONNECTION CREATED")
-
+# print(">>> CREATING NEW CONNECTION...")
+# createNewConnection(name, name, password)
+# print(">>> CONNECTION CREATED")
+# 
 # connect to the wifi network
-print(">>> CONNECTING?")
-connect(name, name)
-print(">>> If you aren't connected to this network, try connecting with the correct password!")
+# print(">>> CONNECTING?")
+# connect(name, name)
+# print(">>> If you aren't connected to this network, try connecting with the correct password!")
 
 
 

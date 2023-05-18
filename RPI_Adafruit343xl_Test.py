4e7bc89a210a35e846ec17f6857e9edcc76498d6
Lyam[BRS] <cous5830@gmail.com>
2023-05-18 16:26:06 -0400

    Accelerometer test now prints addresses detected

--- RPI_Adafruit343xl_Test.py
+++ RPI_Adafruit343xl_Test.py
@@ -4,8 +4,19 @@ import busio
 import adafruit_adxl34x
 
 i2c = busio.I2C(board.SCL, board.SDA)
+
+detected = i2c.scan()
+
+print(f"detected i2c devices: {detected}")
+if(len(detected) == 0):
+    print("No i2c devices were detected... rip...")
+else:
+    print("Something was detected.")
+
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
 while True:
     print("%f %f %f"%accelerometer.acceleration)
     time.sleep(1)
+
+i2c.deinit()

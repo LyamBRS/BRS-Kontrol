096b28bb50812f01e199b5aa730b58e15584f11c
LyamBRS <=>
2023-05-23 11:00:10 -0400

    Extended tests of SerialTester.py

--- RPI_Tests/RPI_SerialTester.py
+++ RPI_Tests/RPI_SerialTester.py
@@ -3,15 +3,16 @@ import time
 
 # Define the serial port configurations
 serial_ports = [
-    "/dev/ttyAMA0",  # Serial 1
-    "/dev/ttyAMA1",  # Serial 2
-    "/dev/ttyAMA2",  # Serial 3
-    "/dev/ttyAMA3",  # Serial 4
-    "/dev/ttyAMA4"   # Serial 5
+    "/dev/ttyAMA0",  # Serial 0
+    "/dev/ttyAMA1",  # Serial 1
+    "/dev/ttyAMA2",  # Serial 2
+    "/dev/ttyAMA3",  # Serial 3
+    "/dev/ttyAMA4",  # Serial 4
+    "/dev/ttyAMA5"   # Serial 5
 ]
 
 # Define the characters to output
-characters = ["A", "B", "C", "D", "E"]
+characters = ["A", "B", "C", "D", "E", "F"]
 
 print("========================================== - [BRS]")
 print("Sending A,B,C,D,E at 9600 baud on serial ports...")
@@ -23,8 +24,8 @@ for port, character in zip(serial_ports, characters):
         ser.close()
         print(f">>> Character {character} sent on {port}")
     except serial.SerialException as e:
-        print(f">>> Error opening {port}: {str(e)}")
-    time.sleep(1)  # Add a delay between sending characters
+        print(f">>> Failed to send on port {port}")
+    time.sleep(0.5)  # Add a delay between sending characters
 
 print("\n")
 print("All characters sent.")

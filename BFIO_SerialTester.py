6314c42ac1a61b020c72e9521b05031b4e488405
LyamBRS <=>
2023-05-25 13:21:15 -0400

    Now printing bytes received

--- BFIO_SerialTester.py
+++ BFIO_SerialTester.py
@@ -4,8 +4,8 @@ from Libraries.BRS_Python_Libraries.BRS.Utilities.bfio import BFIO, NewArrival, 
 
 # Define the serial port configurations
 TIMEOUT = 0.1
-serial_port = "/dev/ttyAMA0"  # Serial 0 -> A -> TX1_B / RX1_B
-# serial_port = "/dev/ttyAMA2"  # Serial 2 -> D -> TX2_A
+# serial_port = "/dev/ttyAMA0"  # Serial 0 -> A -> TX1_B / RX1_B
+serial_port = "/dev/ttyAMA2"  # Serial 2 -> D -> TX2_A
 # serial_port = "/dev/ttyAMA3"  # Serial 3 -> C -> TX1_A / RX1_A
 # serial_port = "/dev/ttyAMA4"  # Serial 4 -> E -> TX2_B / RX2_B
 # serial_port = "/dev/ttyAMA5"  # Serial 5 -> F -> DEBUG / RX2_A
@@ -23,9 +23,10 @@ def GetPassengerArrivals() -> list:
     while serialObject.in_waiting >= 2:
         try:
             data = serialObject.read(2)
+            print(data)
         except:
             print(f"Timed out when trying to read bytes.")
-        passenger = BFIO.GetPassengersFromDualBytes([data[0], data[1]])
+        passenger = BFIO.GetPassengersFromDualBytes(data)
         newArrivals.append(passenger)
     
     return newArrivals

6b7fcd0586703faa85b89fadae413bef195121ea
LyamBRS <=>
2023-05-31 14:09:40 -0400

    Fixed overflow of DebuggerMenu.py

--- Local/Drivers/Batiscan/Pages/DebuggerMenu.py
+++ Local/Drivers/Batiscan/Pages/DebuggerMenu.py
@@ -383,6 +383,8 @@ class DebuggerMenu(Screen):
         self.RecyleBoxLayout.bind(minimum_height=self.RecyleBoxLayout.setter("height"))
 
         self.recycleView = RecycleView()
+        self.recycleView.pos_hint = {"center_x" : 0.5, "center_y" : 0.45}
+        self.recycleView.size_hint = (1,0.90)
         self.recycleView.add_widget(self.RecyleBoxLayout)
         self.recycleView.viewclass = CustomThreeLineIconListItem
         self.Layout.add_widget(self.recycleView)

80a7cc463ccc2e252225efb0d3e2e63168648123
LyamBRS <=>
2023-05-31 09:31:23 -0400

    Attempt to fix issue where crash when loading wifis

--- Programs/Pages/NetworkMenu.py
+++ Programs/Pages/NetworkMenu.py
@@ -6,6 +6,7 @@
 from Libraries.BRS_Python_Libraries.BRS.Debug.LoadingLog import LoadingLog
 from Libraries.BRS_Python_Libraries.BRS.GUI.Status.WiFi import GetWiFiNotAvailableCard, WiFiSelectionCard
 from Libraries.BRS_Python_Libraries.BRS.GUI.Utilities.colors import GetAccentColor
+from Libraries.BRS_Python_Libraries.BRS.Utilities.Enums import Execution
 from Libraries.BRS_Python_Libraries.BRS.Network.WiFi.WiFi import CanDeviceUseWiFi, GetWiFiNetworks
 from Programs.Pages.PopUps import PopUpTypeEnum, PopUps_Screens, PopUpsHandler
 from Programs.Pages.WiFiLogin import WiFiLogin_Screens
@@ -367,11 +368,39 @@ class NetworkMenu(Screen):
             WiFiLogin_Screens.SetBadExiter(NetworkMenu_Screens, "NetworkMenu")
             WiFiLogin_Screens.SetGoodExiter(NetworkMenu_Screens, "NetworkMenu")
 
+            if(network == Execution.Failed):
+                Debug.Error("Fatal error when getting available networks.")
+                Debug.Log("WiFi networks cannot be accessed.")
+                anim = Animation(pos_hint = {"center_x":0.5, "center_y":0.45}, t="in_out_back")
+                anim.start(self.NoWiFiCard)
+                KontrolRGB.DisplayUserError()
+                Debug.End()
+                return
+            
+            if(network == Execution.Crashed):
+                Debug.Error("Fatal error when getting available networks.")
+                Debug.Log("WiFi networks cannot be accessed.")
+                anim = Animation(pos_hint = {"center_x":0.5, "center_y":0.45}, t="in_out_back")
+                anim.start(self.NoWiFiCard)
+                KontrolRGB.DisplayUserError()
+                Debug.End()
+                return
+            
+            if(network == Execution.Incompatibility):
+                Debug.Error("Fatal error when getting available networks.")
+                Debug.Log("WiFi networks cannot be accessed.")
+                anim = Animation(pos_hint = {"center_x":0.5, "center_y":0.45}, t="in_out_back")
+                anim.start(self.NoWiFiCard)
+                KontrolRGB.DisplayUserError()
+                Debug.End()
+                return
+
             Debug.Log("Creating WiFi network cards.")
             for network in networks:
                 WiFiCard = WiFiSelectionCard(network)
                 WiFiCard.PressedEnd = self.GoToWiFiConnectionScreen
                 self.cardBox.add_widget(WiFiCard)
+            
             KontrolRGB.DisplayDefaultColor()
 
         Debug.Log("Starting WiFi Updater")
